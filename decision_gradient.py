747416346bcd801e083a8e926e6c65ac462cc3f3
Emily Lewis <emilymlewis0@gmail.com>
2025-04-25 10:25:56 -0400

    Adjust imports

--- decision_gradient.py
+++ decision_gradient.py
@@ -2,10 +2,12 @@ import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, f1_score
+from sklearn.ensemble import GradientBoostingClassifier
+
 from tools.train_test import get_split
 
-X_train, X_test, y_train, y_test = get_split('./final_preprocessed_data.csv')
+X_train, X_test, y_train, y_test = get_split('./final_preprocessed_data.csv', use_alt_preprocessing=False)
 
 # Define hyperparameter grid
 param_grid = {
@@ -26,8 +28,6 @@ print("Best Decision Tree Parameters:", grid_search_dt.best_params_)
 y_pred_dt = best_dt.predict(X_test)
 print("Decision Tree Performance:\n", classification_report(y_test, y_pred_dt))
 
-from sklearn.ensemble import GradientBoostingClassifier
-
 # Define hyperparameter grid
 param_grid = {
     'n_estimators': [100, 300],  # Number of trees
@@ -46,8 +46,6 @@ print("Best Gradient Boosting Parameters:", grid_search_gb.best_params_)
 
 y_pred_gb = best_gb.predict(X_test)
 print("Gradient Boosting Performance:\n", classification_report(y_test, y_pred_gb))
-
-from sklearn.metrics import accuracy_score, f1_score
 
 # Compute scores
 dt_f1 = f1_score(y_test, y_pred_dt)
